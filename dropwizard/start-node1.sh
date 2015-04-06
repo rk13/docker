1f60794f1e3b72fec8e9c0092658bcbd0042a430
root <root@static.200.80.76.144.clients.your-server.de>
2015-04-06 02:28:07 +0200

    Dropwizard haproxy configuration

---
+++ dropwizard/start-node1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --name dropwizard-example-node-1 -d -p 18443:8443 dropwizard-example
+
---
+++ dropwizard/start-node2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --name dropwizard-example-node-2 -d -p 28443:8443 dropwizard-example
+
